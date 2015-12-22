15be8b3b1c3053e6f1e79e7c48ed7caea0347323
Dominik Weber <accounts@dominik-weber.name>
2015-12-22 13:13:38 +0100

    Add public facing Resolver class and simplest path resolving via properties

---
+++ src/Pather.CSharp/Resolver.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace Pather.CSharp
+{
+    public class Resolver
+    {
+        public object Resolve(object target, string path)
+        {
+            var pathElements = path.Split('.');
+            var tempResult = target;
+            foreach(var pathElement in pathElements)
+            {
+                PropertyInfo p = tempResult.GetType().GetProperty(pathElement);
+                if (p == null)
+                    throw new ArgumentException($"The property {pathElement} could not be found.");
+
+                tempResult = p.GetValue(tempResult);
+            }
+            var result = tempResult;
+            return result;
+        }
+    }
+}
