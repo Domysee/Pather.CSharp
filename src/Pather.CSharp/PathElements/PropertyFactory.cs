2de560d6644a7f73aa6e5b35cd5e0254d02c05db
Dominik Weber <accounts@dominik-weber.name>
2015-12-24 13:53:15 +0100

    Add a factory for Property path element

---
+++ src/Pather.CSharp/PathElements/PropertyFactory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Pather.CSharp.PathElements
+{
+    public class PropertyFactory : IPathElementFactory
+    {
+        public IPathElement Create(string pathElement)
+        {
+            return new Property(pathElement);
+        }
+
+        public bool IsApplicable(string pathElement)
+        {
+            return Regex.IsMatch(pathElement, @"\w+");
+        }
+    }
+}
