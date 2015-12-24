f7828e0e653338ab34a35d72c6b37d15142b9996
Dominik Weber <accounts@dominik-weber.name>
2015-12-24 13:41:02 +0100

    Factor out the check if a type is applicable to a given pathElement string into a separate method

--- src/Pather.CSharp/Resolver.cs
+++ src/Pather.CSharp/Resolver.cs
@@ -36,24 +36,26 @@ namespace Pather.CSharp
         private object createPathElement(string pathElement)
         {
             //get the first applicable path element type
-            var pathElementType = pathElementTypes.Where(t =>
-            {
-                MethodInfo applicableMethod = t.GetMethod("IsApplicable", BindingFlags.Static | BindingFlags.Public);
-                if (applicableMethod == null)
-                    throw new InvalidOperationException($"The type {t.Name} does not have a static method IsApplicable");
-
-                bool? applicable = applicableMethod.Invoke(null, new[] { pathElement }) as bool?;
-                if (applicable == null)
-                    throw new InvalidOperationException($"IsApplicable of type {t.Name} does not return bool");
-
-                return applicable.Value;
-            }).FirstOrDefault();
+            var pathElementType = pathElementTypes.Where(t => isApplicable(t, pathElement)).FirstOrDefault();
 
             if (pathElementType == null)
                 throw new InvalidOperationException($"There is no applicable path element type for {pathElement}");
 
             var result = Activator.CreateInstance(pathElementType, pathElement); //each path element type must have a constructor that takes a string parameter
             return result;
+        }
+
+        private bool isApplicable(Type t, string pathElement)
+        {
+            MethodInfo applicableMethod = t.GetMethod("IsApplicable", BindingFlags.Static | BindingFlags.Public);
+            if (applicableMethod == null)
+                throw new InvalidOperationException($"The type {t.Name} does not have a static method IsApplicable");
+
+            bool? applicable = applicableMethod.Invoke(null, new[] { pathElement }) as bool?;
+            if (applicable == null)
+                throw new InvalidOperationException($"IsApplicable of type {t.Name} does not return bool");
+
+            return applicable.Value;
         }
     }
 }
