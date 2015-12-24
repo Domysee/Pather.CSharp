bc9ba6eeefcf89cac40ab4b7faa9783be79889f3
Dominik Weber <accounts@dominik-weber.name>
2015-12-24 13:51:33 +0100

    Add interface IPathElementFactory
    This is the start of a refactoring to use instances of classes to determine if a path element type
    is applicable and to create instances of IPathElement types.

---
+++ src/Pather.CSharp/PathElements/IPathElementFactory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pather.CSharp.PathElements
+{
+    public interface IPathElementFactory
+    {
+        bool IsApplicable(string pathElement);
+        IPathElement Create(string pathElement);
+    }
+}
