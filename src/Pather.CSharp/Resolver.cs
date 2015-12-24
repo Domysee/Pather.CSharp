89afbe0362f3629ac200ffac51083c2fcc301b9c
Dominik Weber <accounts@dominik-weber.name>
2015-12-24 13:57:03 +0100

    Refactor Resolver to use path element factories

--- src/Pather.CSharp/Resolver.cs
+++ src/Pather.CSharp/Resolver.cs
@@ -9,12 +9,12 @@ namespace Pather.CSharp
 {
     public class Resolver
     {
-        private IList<Type> pathElementTypes;
+        private IList<IPathElementFactory> pathElementTypes;
 
         public Resolver()
         {
-            pathElementTypes = new List<Type>();
-            pathElementTypes.Add(typeof(Property));
+            pathElementTypes = new List<IPathElementFactory>();
+            pathElementTypes.Add(new PropertyFactory());
         }
 
         public object Resolve(object target, string path)
@@ -33,26 +33,18 @@ namespace Pather.CSharp
         private IPathElement createPathElement(string pathElement)
         {
             //get the first applicable path element type
-            var pathElementType = pathElementTypes.Where(t => isApplicable(t, pathElement)).FirstOrDefault();
+            var pathElementFactory = pathElementTypes.Where(f => isApplicable(f, pathElement)).FirstOrDefault();
 
-            if (pathElementType == null)
+            if (pathElementFactory == null)
                 throw new InvalidOperationException($"There is no applicable path element type for {pathElement}");
 
-            var result = Activator.CreateInstance(pathElementType, pathElement); //each path element type must have a constructor that takes a string parameter
-            return result as IPathElement;
+            IPathElement result = pathElementFactory.Create(pathElement);
+            return result;
         }
 
-        private bool isApplicable(Type t, string pathElement)
+        private bool isApplicable(IPathElementFactory factory, string pathElement)
         {
-            MethodInfo applicableMethod = t.GetMethod("IsApplicable", BindingFlags.Static | BindingFlags.Public);
-            if (applicableMethod == null)
-                throw new InvalidOperationException($"The type {t.Name} does not have a static method IsApplicable");
-
-            bool? applicable = applicableMethod.Invoke(null, new[] { pathElement }) as bool?;
-            if (applicable == null)
-                throw new InvalidOperationException($"IsApplicable of type {t.Name} does not return bool");
-
-            return applicable.Value;
+            return factory.IsApplicable(pathElement);
         }
     }
 }
