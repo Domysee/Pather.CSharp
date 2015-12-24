84606104f142332453a1f8094277ce7f173b8511
Dominik Weber <accounts@dominik-weber.name>
2015-12-24 13:36:42 +0100

    Add Property path element type
    Each path element will be represented by an instance of a type.
    If the type is applicable for the given path element (string) is determined by the static IsApplicable method.
    The Apply method applies the path element on the given object.

---
+++ src/Pather.CSharp/PathElements/Property.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Pather.CSharp.PathElements
+{
+    public class Property
+    {
+        public static bool IsApplicable(string pathElement)
+        {
+            return Regex.IsMatch(pathElement, @"\w+");
+        }
+
+        private string property;
+
+        public Property(string property)
+        {
+            this.property = property;
+        }
+
+        public object Apply(object target)
+        {
+            PropertyInfo p = target.GetType().GetProperty(property);
+            if (p == null)
+                throw new ArgumentException($"The property {property} could not be found.");
+
+            var result = p.GetValue(target);
+            return result;
+        }
+    }
+}
