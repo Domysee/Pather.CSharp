771532bcb4bd8ef84a8742a789ad8b36f4979b8d
Dominik Weber <accounts@dominik-weber.name>
2015-12-25 19:39:30 +0100

    Add factory for the PathElement class for array like access

---
+++ src/Pather.CSharp/PathElements/EnumerableAccessFactory.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Pather.CSharp.PathElements
+{
+    public class EnumerableAccessFactory : IPathElementFactory
+    {
+        public IPathElement Create(string pathElement)
+        {
+            var matches = Regex.Matches(pathElement, @"^(\w+)\[(\d+)\]$");
+            Match match = matches[0];
+            //0 is the whole match
+            string property = match.Captures[1].Value;
+            int index = int.Parse(match.Captures[2].Value); //the regex guarantees that the second group is an integer, so no further check is needed
+
+            return new EnumerableAccess(property, index);
+        }
+
+        public bool IsApplicable(string pathElement)
+        {
+            return Regex.IsMatch(pathElement, @"^\w+\[\d+\]$");
+        }
+    }
+}
