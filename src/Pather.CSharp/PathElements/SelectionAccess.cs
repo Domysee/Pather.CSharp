09f0be7f392ec0d50bdca80e5d7d113a509c9071
Dominik Weber <accounts@dominik-weber.name>
2015-12-27 21:50:28 +0100

    Add path element class for selection access

---
+++ src/Pather.CSharp/PathElements/SelectionAccess.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace Pather.CSharp.PathElements
+{
+    public class SelectionAccess : IPathElement
+    {
+        public SelectionAccess()
+        {
+        }
+
+        public object Apply(object target)
+        {
+            var enumerable = target as IEnumerable;
+            var result = new Selection(enumerable);
+            return result;
+        }
+    }
+}
