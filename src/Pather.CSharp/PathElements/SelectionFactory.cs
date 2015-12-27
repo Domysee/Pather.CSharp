b3dfe626ae3efda24a8929a50a2c2b25f497f5fc
Dominik Weber <accounts@dominik-weber.name>
2015-12-27 21:50:40 +0100

    Add path element factory for selection access

---
+++ src/Pather.CSharp/PathElements/SelectionFactory.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Pather.CSharp.PathElements
+{
+    public class SelectionFactory : IPathElementFactory
+    {
+        private const string selectionIndicator = "[]";
+        public IPathElement Create(string path, out string newPath)
+        {
+            newPath = path.Remove(0, selectionIndicator.Length);
+            return new SelectionAccess();
+        }
+
+        public bool IsApplicable(string path)
+        {
+            return path.StartsWith(selectionIndicator);
+        }
+    }
+}
