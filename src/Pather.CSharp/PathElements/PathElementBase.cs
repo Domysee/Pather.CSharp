92e9075589b2f9ba647d148b2a35c8e468f502cf
Dominik Weber <accounts@dominik-weber.name>
2015-12-27 21:57:46 +0100

    Add base class for path elements that has a good default implementation for Apply on Selection

---
+++ src/Pather.CSharp/PathElements/PathElementBase.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pather.CSharp.PathElements
+{
+    public abstract class PathElementBase : IPathElement
+    {
+        public Selection Apply(Selection target)
+        {
+            var results = new List<object>();
+            foreach (var entriy in target.Entries)
+            {
+                results.Add(Apply(entriy));
+            }
+            var result = new Selection(results);
+            return result;
+        }
+
+        public abstract object Apply(object target);
+    }
+}
