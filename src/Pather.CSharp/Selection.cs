610bea79191ffb213a771aa3a68339608fef49f6
Dominik Weber <accounts@dominik-weber.name>
2015-12-27 21:49:52 +0100

    Add Selection class
    A selection contains a collection of objects. It is necessary to be able to distinguish it from other
    collection classes (like IEnumerable), so that it is known that the resolving should be applied
    to all items of the collection.

---
+++ src/Pather.CSharp/Selection.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pather.CSharp
+{
+    public class Selection
+    {
+        public IReadOnlyCollection<object> Entries { get; }
+
+        public Selection(IEnumerable entries)
+        {
+            var list = new List<object>();
+            foreach (var entry in entries)
+                list.Add(entry);
+            Entries = list;
+        }
+    }
+}
