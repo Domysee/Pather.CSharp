0a3dbd5f4f79fe5a7845d8e27af5427abc444dfd
Dominik Weber <accounts@dominik-weber.name>
2015-12-27 22:03:26 +0100

    Implement the apply method for selections on SelectionAccess to flatten the selection
    If the selection contains collections, they are flattened into a new collection.

--- src/Pather.CSharp/PathElements/SelectionAccess.cs
+++ src/Pather.CSharp/PathElements/SelectionAccess.cs
@@ -19,5 +19,23 @@ namespace Pather.CSharp.PathElements
             var result = new Selection(enumerable);
             return result;
         }
+
+        public Selection Apply(Selection target)
+        {
+            var results = new List<object>();
+            foreach(var entry in target.Entries)
+            {
+                var enumerable = entry as IEnumerable;
+                if (enumerable == null)
+                    results.Add(entry);
+                else
+                {
+                    foreach (var element in enumerable)
+                        results.Add(element);
+                }
+            }
+            var result = new Selection(results);
+            return result;
+        }
     }
 }
