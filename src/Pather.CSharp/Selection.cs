5a3f36ccbc9317bfbc7ee3654684261209c270d9
Dominik Weber <accounts@dominik-weber.name>
2015-12-28 19:22:37 +0100

    Change Selection to implement IEnumerable and add an Enumerator for Selection
    This way it is easily possible to iterator over the elements of a Selection, without having
    to know that it has an Entries property.

--- src/Pather.CSharp/Selection.cs
+++ src/Pather.CSharp/Selection.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Pather.CSharp
 {
-    internal class Selection
+    public class Selection : IEnumerable, IEnumerable<object>
     {
         public IReadOnlyCollection<object> Entries { get; }
 
@@ -16,6 +16,16 @@ namespace Pather.CSharp
             foreach (var entry in entries)
                 list.Add(entry);
             Entries = list;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return new SelectionEnumerator(this);
+        }
+
+        IEnumerator<object> IEnumerable<object>.GetEnumerator()
+        {
+            return new SelectionEnumerator(this);
         }
     }
 }
---
+++ src/Pather.CSharp/SelectionEnumerator.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pather.CSharp
+{
+    public class SelectionEnumerator : IEnumerator, IEnumerator<object>
+    {
+        private Selection selection;
+        private int currentIndex;
+
+        public SelectionEnumerator(Selection selection)
+        {
+            this.selection = selection;
+            currentIndex = -1;
+        }
+
+        public object Current
+        {
+            get
+            {
+                lock (selection)
+                {
+                    if (currentIndex == -1) return null;
+                    if (currentIndex >= selection.Entries.Count)
+                        throw new InvalidOperationException("The enumerator is past the last element. Call Reset to start again from the first one");
+                    return selection.Entries.ElementAt(currentIndex);
+                }
+            }
+        }
+
+        public bool MoveNext()
+        {
+            currentIndex += 1;
+            return currentIndex < selection.Entries.Count;
+        }
+
+        public void Reset()
+        {
+            currentIndex = -1;
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
