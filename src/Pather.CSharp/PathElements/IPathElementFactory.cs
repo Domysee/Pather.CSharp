39c1b944bc48010902a227f2481885eb3d408e4d
Dominik Weber <accounts@dominik-weber.name>
2016-08-26 20:26:26 +0200

    Add Documentation to IPathElementFactory Methods

--- src/Pather.CSharp/PathElements/IPathElementFactory.cs
+++ src/Pather.CSharp/PathElements/IPathElementFactory.cs
@@ -7,7 +7,19 @@ namespace Pather.CSharp.PathElements
 {
     public interface IPathElementFactory
     {
+        /// <summary>
+        /// checks if the factory can create a path element from the given path
+        /// </summary>
+        /// <param name="path"></param>
+        /// <returns></returns>
         bool IsApplicable(string path);
+
+        /// <summary>
+        /// creates a path element from the given path
+        /// </summary>
+        /// <param name="path"></param>
+        /// <param name="newPath">outputs the path removed by the bit that was used to create the path element</param>
+        /// <returns></returns>
         IPathElement Create(string path, out string newPath);
     }
 }
