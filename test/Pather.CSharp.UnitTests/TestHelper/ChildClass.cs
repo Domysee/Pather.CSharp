eb0e5f4325eed2d3f54868b66d451bd7e23de478
Dominik Weber <accounts@dominik-weber.name>
2017-04-29 10:12:53 +0200

    Add Test for resolving Base Class Properties

---
+++ test/Pather.CSharp.UnitTests/TestHelper/ChildClass.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pather.CSharp.UnitTests.TestHelper
+{
+    class ChildClass : BaseClass
+    {
+        public string Property2 { get; set; }
+    }
+}
